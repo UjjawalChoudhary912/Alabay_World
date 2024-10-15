d835648f057be7e37b1d06a2667d058ed9e6ded6
Ujjawal Choudhary <ujjawalchoudhary912@gmail.com>
2024-10-16 01:25:38 +0530

    commit-4

--- alabay_world/src/App.js
+++ alabay_world/src/App.js
@@ -1,5 +1,5 @@
-import firstpicture from "./components/Images/firstpicture.png"
-import './App.css'
+import firstpicture from "./components/Images/firstpicture.png";
+import './App.css';
 import Page1 from "./components/Page1";
 import Page2 from "./components/Page2";
 import Page3 from "./components/Page3";
@@ -12,16 +12,18 @@ import Page8 from "./components/Page8";
 function App() {
   return (
     <div>
-     <img className="Background" src={firstpicture} alt={firstpicture}/>
-     <Page1/>
-     <Page2/>
-     <Page3/>
-     <Page4/>
-     <Page5/>
-     <Page6/>
-     <Page7/>
-     <Page8/>
- </div>
+      <img className="Background" src={firstpicture} alt={firstpicture} />
+      <div className="page-container">
+        <Page1 />
+        <Page2 />
+        <Page3 />
+        <Page4 />
+        <Page5 />
+        <Page6 />
+        <Page7 />
+        <Page8 />
+      </div>
+    </div>
   );
 }
 
--- alabay_world/src/components/Page6.js
+++ alabay_world/src/components/Page6.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Page6.css";
 import blackHoodiebck from './Images/Alabay Merch/black hoodie bck.png';
 import blackHoodie from './Images/Alabay Merch/black hoodie.png';
@@ -11,6 +11,7 @@ import yellowTee from './Images/Alabay Merch/yellow tshirt.png';
 
 const Page6 = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [isAutoSliding, setIsAutoSliding] = useState(true);
 
   const merchandise = [
     { bck: blackHoodiebck, front: blackHoodie },
@@ -27,16 +28,36 @@ const Page6 = () => {
     setCurrentSlide((prevSlide) => (prevSlide - 1 + merchandise.length) % merchandise.length);
   };
 
+  useEffect(() => {
+    if (!isAutoSliding) return;
+    
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 2000);
+    
+    return () => clearInterval(interval);
+  }, [isAutoSliding, currentSlide]);
+
+  const handleNext = () => {
+    setIsAutoSliding(false); // Stop auto sliding
+    nextSlide();
+  };
+
+  const handlePrev = () => {
+    setIsAutoSliding(false); // Stop auto sliding
+    prevSlide();
+  };
+
   return (
     <div className='pg6'>
       <h1>MERCHANDISE</h1>
       <div className='slider'>
-        <button className='prev' onClick={prevSlide}>&lt;</button>
+        <button className='prev' onClick={handlePrev}>&lt;</button>
         <div className='merch-item'>
           <img className='bck-image' src={merchandise[currentSlide].bck} alt='Background' />
           <img className='front-image' src={merchandise[currentSlide].front} alt='Merchandise' />
         </div>
-        <button className='next' onClick={nextSlide}>&gt;</button>
+        <button className='next' onClick={handleNext}>&gt;</button>
       </div>
     </div>
   );
